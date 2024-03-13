080a12da203e96d7759064c52227ea4e42cae119
chrisroederucdenver <47036053+chrisroederucdenver@users.noreply.github.com>
2024-03-13 11:30:12 -0600

    started better organization, adding a function that returns a structure instead of just printing in person.py

--- person.py
+++ person.py
@@ -6,6 +6,21 @@ import id_map
 f = open("input_json/patient.input.json")
 data = json.load(f)
 
+def print_convert():
+    location_key = (
+        data["address"][0]["line"],
+        data["address"][0]["city"],
+        data["address"][0]["state"],
+        data["address"][0]["postalCode"])
+    location_id = id_map.get(location_key)
+
+    print("person_id:  ", data["id"])
+    print("race:       ", data["extension"][0]["extension"][0]["valueCoding"]["display"])
+    print("ethnicity:  ", data["extension"][1]["extension"][0]["valueCoding"]["display"])
+    print("gender:     ", data["gender"])
+    print("birthdate:  ", data["birthDate"])
+    print("location_id:", location_id)
+
 def convert():
     location_key = (
         data["address"][0]["line"],
@@ -14,12 +29,26 @@ def convert():
         data["address"][0]["postalCode"])
     location_id = id_map.get(location_key)
 
-    print("person_id:  ", data["id"]);
-    print("race:       ", data["extension"][0]["extension"][0]["valueCoding"]["display"])
-    print("ethnicity:  ", data["extension"][1]["extension"][0]["valueCoding"]["display"])
-    print("gender:     ", data["gender"])
-    print("birthdate:  ", data["birthDate"])
-    print("location_id:", location_id)
+    dest = {}
+    dest.id = data["id"]
+    dest.race = data["extension"][0]["extension"][0]["valueCoding"]["display"]
+    dest.ethnicity = data["extension"][1]["extension"][0]["valueCoding"]["display"]
+    dest.gender = data["gender"]
+    dest.birthdate = data["birthDate"]
+    dest.location_id = location_id
 
+    return dest
 
+def play():
+    # if the output structure is deeper, you have to generate/create/"vivify" the intermediate structures.
+    # contrived, but like this:
+    # (Perl implicitly does the creation with only something similar to the last line.)
+    dest = { "part_a": null, "part_b": null, "part_c": null }
+    dest.part_a = [null, null, null]
+    dest.part_a[1] = {"person": null, "addr": null, "event":null}
+    dest.part_a[1].person = convert()}
+    
+    # The upshot of auto-vivification in Perl (as if it matters), is that you can
+    # assign from a source path to a destination path with minimal extra code.
 
+    return dest
